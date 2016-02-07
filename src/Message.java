9dc62f72b4aa28335fdf2ba56dc92baace05b4dc
Pr0x1m4 <proxima.aust@gmail.com>
2016-02-07 18:38:05 -0500

    testing egit

--- src/Message.java
+++ src/Message.java
@@ -3,5 +3,6 @@ import java.io.Serializable;
 public class Message implements Serializable{
 	public static void main(String[] args) {
 		System.out.println("Test");
+		System.out.println("testing egit");
 	}
 }
